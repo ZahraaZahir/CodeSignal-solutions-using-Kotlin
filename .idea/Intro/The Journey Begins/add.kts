a16fe14e26962e0ac56883b3cf2e244651906358
Zahraa Zahir <ZahraaZahiirr@gmail.com>
2021-07-29 17:48:24 +0300

    add solutions of intro/ the journey begins

---
+++ .idea/Intro/The Journey Begins/add.kts
@@ -0,0 +1,6 @@
+/*The question is:
+ Write a function that returns the sum of two numbers.
+*/
+
+//the combact function in Kotlin is wonderful!
+fun add(param1:Int, param2:Int): Int = param1 + param2
---
+++ .idea/Intro/The Journey Begins/checkPalindrome.kts
@@ -0,0 +1,5 @@
+/*The question is:
+ Given the string, check if it is a palindrome.
+*/
+
+fun checkPalindrome(inputString:String) = inputString.equals(inputString.reversed())
